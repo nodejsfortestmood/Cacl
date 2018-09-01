4f24a4fc313b368847baa01b1543ceee76a5d452
testmood <testmood@163.com>
2018-09-01 21:53:50 +0800

    sdf

--- Cacl/src/main/java/org/test/demo/Cacl/App.java
+++ Cacl/src/main/java/org/test/demo/Cacl/App.java
@@ -10,6 +10,9 @@ public class App
     {
         System.out.println( "Hello World! in anather" );
         System.out.println( "Hello World!" );
+        for(int i=0;i<100;i++){
+        	System.out.println(i);
+        }
         System.out.println();
     }
 }
