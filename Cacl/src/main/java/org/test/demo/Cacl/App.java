33d6699d9ad981371d44120867ecf644cb9f22c5
testmood <testmood@163.com>
2018-09-01 21:59:29 +0800

    okkk

--- Cacl/src/main/java/org/test/demo/Cacl/App.java
+++ Cacl/src/main/java/org/test/demo/Cacl/App.java
@@ -26,11 +26,29 @@ public class App
         	System.out.println(i);
         }
         System.out.println();
-        for(int i=0;i<200;i++){
+        for(int i=0;i<2060;i++){
+        	System.out.println(i);
+        }
+        System.out.println();
+        System.out.println( "Hello Wsorld! in anather" );
+        System.out.println( "Hello World!" );
+        for(int i=0;i<100;i++){
+        	System.out.println(i);
+        }
+        System.out.println();
+        System.out.println( "Hello World! in ansather" );
+        System.out.println( "Hello World!s" );
+        for(int i=0;i<100;i++){
         	System.out.println(i);
         }
         System.out.println();
         System.out.println( "Hello World! in anather" );
+        System.out.println( "Hello World!" );
+        for(int i=0;i<100;i++){
+        	System.out.println(i);
+        }
+        System.out.println();
+        System.out.println( "Hello Worlsd! in anather" );
         System.out.println( "Hello World!" );
         for(int i=0;i<100;i++){
         	System.out.println(i);
@@ -42,7 +60,7 @@ public class App
         	System.out.println(i);
         }
         System.out.println();
-        System.out.println( "Hello World! in anather" );
+        System.out.println( "Hello sWorld! in ansather" );
         System.out.println( "Hello World!" );
         for(int i=0;i<100;i++){
         	System.out.println(i);
@@ -54,43 +72,25 @@ public class App
         	System.out.println(i);
         }
         System.out.println();
-        System.out.println( "Hello World! in anather" );
+        System.out.println( "Hello World! in asnather" );
         System.out.println( "Hello World!" );
         for(int i=0;i<100;i++){
         	System.out.println(i);
         }
         System.out.println();
-        System.out.println( "Hello World! in anather" );
+        System.out.println( "Hello World! in ansather" );
         System.out.println( "Hello World!" );
         for(int i=0;i<100;i++){
         	System.out.println(i);
         }
         System.out.println();
-        System.out.println( "Hello World! in anather" );
+        System.out.println( "Hello World! in ansather" );
         System.out.println( "Hello World!" );
         for(int i=0;i<100;i++){
         	System.out.println(i);
         }
         System.out.println();
-        System.out.println( "Hello World! in anather" );
-        System.out.println( "Hello World!" );
-        for(int i=0;i<100;i++){
-        	System.out.println(i);
-        }
-        System.out.println();
-        System.out.println( "Hello World! in anather" );
-        System.out.println( "Hello World!" );
-        for(int i=0;i<100;i++){
-        	System.out.println(i);
-        }
-        System.out.println();
-        System.out.println( "Hello World! in anather" );
-        System.out.println( "Hello World!" );
-        for(int i=0;i<100;i++){
-        	System.out.println(i);
-        }
-        System.out.println();
-        System.out.println( "Hello World! in anather" );
+        System.out.println( "Hello World! in anatsher" );
         System.out.println( "Hello World!" );
         for(int i=0;i<100;i++){
         	System.out.println(i);
