48d4c5684e2dcae3d7de928dbd7d7ae0fc6865ee
testmood <testmood@163.com>
2018-09-04 23:08:14 +0800

    test

--- Cacl/src/main/java/org/test/demo/Cacl/Test.java
+++ Cacl/src/main/java/org/test/demo/Cacl/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("ok");
 		System.out.println("this not a kay made by hans in master");
+		System.out.println("this not a kay made by hans in master");
 	}
 
 }
